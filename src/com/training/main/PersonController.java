f87fda4ce987f7d8b65b70675f2b467f03040b1b
Carla Gabrielle Motin <carlamotin@gmail.com>
2021-03-17 15:05:42 -0300

    fix2

--- src/com/training/main/PersonController.java
+++ src/com/training/main/PersonController.java
@@ -7,14 +7,15 @@ public class PersonController {
     public Person addPerson(Person person) {
         Person newPerson = new Person();
 
-        if(!person.getName().isEmpty()) {
+        if(person.getName() != null) {
             newPerson.setId(person.getId());
             newPerson.setName(person.getName());
             newPerson.setAge(person.getAge());
 
             System.out.println("New Person Added!");
+            return newPerson;
         }
 
-        return newPerson;
+        return null;
     }
 }
